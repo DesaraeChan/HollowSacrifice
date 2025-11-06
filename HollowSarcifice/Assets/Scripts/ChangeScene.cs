5fc80e152eabd9820d1bb31b08f386273fe8c5c1
DesaraeChan <desmond_chen@sfu.ca>
2025-11-06 00:24:54 -0800

    Fade In and out transition

--- HollowSarcifice/Assets/Scripts/ChangeScene.cs
+++ HollowSarcifice/Assets/Scripts/ChangeScene.cs
@@ -1,10 +1,28 @@
 using UnityEngine;
+using System.Collections;
 using UnityEngine.SceneManagement;
 public class ChangeScene : MonoBehaviour
 {
+    public Fading fade;
+
+    void Start()
+    {
+         fade = FindFirstObjectByType<Fading>();
+    }
+
+
+    public IEnumerator _ChangeScene()
+    {
+        fade.FadeIn();
+        yield return new WaitForSeconds(1);
+        SceneManager.LoadScene("Hypothetical2DWalking");
+        
+    }
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public void GoToSceneTwo()
     {
-        SceneManager.LoadScene("Hypothetical2DWalking");
+        StartCoroutine(_ChangeScene());
     }
 }
---
+++ HollowSarcifice/Assets/Scripts/Controller.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+
+    Fading fade;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        fade = FindFirstObjectByType<Fading>();
+
+        fade.FadeOut();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
