1f6e82a5ce06f0409bc9f5028ebdc068aaa67e62
DesaraeChan <desmond_chen@sfu.ca>
2025-11-05 16:01:57 -0800

    Movement implemented
    Basic floors and walls active

---
+++ HollowSarcifice/Assets/Player Character/PlayerMovement.cs
@@ -0,0 +1,87 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public Rigidbody2D body;
+    public float speed = 3;
+    public float acceleration = 1.1f;
+    [Range(0f, 1f)]
+    public float groundDecay;
+    public BoxCollider2D groundCheck;
+
+    public LayerMask groundMask;
+    float xInput;
+    float yInput;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetInput();
+        moveWithInput();
+
+      
+
+       
+    }
+
+    void FixedUpdate()
+    {
+        ApplyFriction();
+        
+    }
+
+    void GetInput()
+    {
+        xInput = Input.GetAxis("Horizontal");
+        yInput = Input.GetAxis("Vertical");
+    }
+
+    void moveWithInput()
+    {
+        if (Mathf.Abs(xInput) > 0)
+        {
+
+            float increment = xInput * acceleration;
+            float newSpeed = Mathf.Clamp(body.linearVelocity.x + increment, -speed, speed);
+
+            body.linearVelocity = new Vector2(newSpeed, body.linearVelocity.y);
+
+            float direction = Mathf.Sign(xInput);
+            transform.localScale = new Vector3(direction, 1, 1);
+        }
+
+
+    }
+    
+    // void handleJump()
+    // {
+    //     if (Mathf.Abs(yInput) > 0 && CheckGround())
+    //     {
+    //         body.linearVelocity = new Vector2(body.linearVelocity.x, yInput * speed);
+
+    //     }
+    // }
+
+    void ApplyFriction()
+    {
+        if (CheckGround() && xInput == 0)
+        {
+            body.linearVelocity *= groundDecay;
+        }
+    }
+
+    bool CheckGround()
+    {
+        return Physics2D.OverlapAreaAll(groundCheck.bounds.min, groundCheck.bounds.max, groundMask).Length > 0;
+    }
+
+}
+
