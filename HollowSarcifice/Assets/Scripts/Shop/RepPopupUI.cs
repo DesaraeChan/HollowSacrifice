7455858cb009114d1af0a314bae6bf8c99253540
Ned-200 <nadine.elkabir@gmail.com>
2025-11-16 18:40:54 -0800

    rep popup fix

--- HollowSarcifice/Assets/Scripts/Shop/RepPopupUI.cs
+++ HollowSarcifice/Assets/Scripts/Shop/RepPopupUI.cs
@@ -1,16 +1,26 @@
 using UnityEngine;
 using TMPro;
-
 public class RepPopupUI : MonoBehaviour
 {
     [SerializeField] private TMP_Text amountText;
     [SerializeField] private Animator animator;
 
-    void OnEnable()  => CharacterManager.OnReputationApplied += HandleRep;
-    void OnDisable() => CharacterManager.OnReputationApplied -= HandleRep;
+    void OnEnable()
+    {
+        Debug.Log("[RepPopupUI] Enabled, subscribing to OnReputationApplied");
+        CharacterManager.OnReputationApplied += HandleRep;
+    }
+
+    void OnDisable()
+    {
+        Debug.Log("[RepPopupUI] Disabled, unsubscribing from OnReputationApplied");
+        CharacterManager.OnReputationApplied -= HandleRep;
+    }
 
     private void HandleRep(int delta)
     {
+        Debug.Log($"[RepPopupUI] HandleRep called with delta = {delta}");
+
         if (delta == 0) return;
 
         if (amountText){
@@ -18,9 +28,8 @@ public class RepPopupUI : MonoBehaviour
                 amountText.text = $"+{delta} Rep Point";
             else
                 amountText.text = $"{delta} Rep Point";
-            
         }
 
-        if (animator)   animator.SetTrigger(delta > 0 ? "Up" : "Down");
+        if (animator) animator.SetTrigger(delta > 0 ? "Up" : "Down");
     }
 }
